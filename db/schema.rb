b231408d7f97a3ae847b37fa7b7894d17f3aff8f
shimiyou <0ri6ku1jo9apple@gmail.com>
2019-12-15 22:36:33 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_18_173353) do
+ActiveRecord::Schema.define(version: 2019_12_15_133413) do
 
   create_table "albums", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
