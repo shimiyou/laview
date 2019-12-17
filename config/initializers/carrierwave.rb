3a80dfb84884d41421db8d2607110c7d3b710229
shimiyou <0ri6ku1jo9apple@gmail.com>
2019-12-17 23:17:36 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,12 +10,12 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.aws_access_key_id,
-      aws_secret_access_key: Rails.application.credentials.aws_secret_access_key,
-      region: 'ap-northeast-1'
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: ENV['S3_REGION']
     }
 
-    config.fog_directory  = 'laviewapp'
+    config.fog_directory  = ENV['S3_BUCKET']
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/laviewapp'
   end
 end
