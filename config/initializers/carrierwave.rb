81d8732642a1e192656987f27795b00eefc8b692
shimiyou <0ri6ku1jo9apple@gmail.com>
2019-12-17 21:57:54 +0900

    mysql

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,12 +10,13 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AWS_S3_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_S3_SECRET_ACCESS_KEY'],
-      region: ENV['AWS_S3_REGION']
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: ENV['AWS_DEFAULT_REGION'],
+      path_style: true
     }
 
-    config.fog_directory  = ENV['AWS_S3_BUCKET']
+    config.fog_directory  = 'laviewapp'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/laviewapp'
   end
 end
