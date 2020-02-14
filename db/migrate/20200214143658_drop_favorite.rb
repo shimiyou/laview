1cadee619866d0ed120b32a9f418ca7a8b13c0ec
shimiyou <0ri6ku1jo9apple@gmail.com>
2020-02-14 23:38:55 +0900

    favorite_table_destroy

---
+++ db/migrate/20200214143658_drop_favorite.rb
@@ -0,0 +1,5 @@
+class DropFavorite < ActiveRecord::Migration[5.2]
+  def change
+    drop_table :favorites
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_27_082906) do
+ActiveRecord::Schema.define(version: 2020_02_14_143658) do
 
   create_table "albums", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -18,13 +18,6 @@ ActiveRecord::Schema.define(version: 2019_12_27_082906) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.string "color", null: false
-  end
-
-  create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "album_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["album_id"], name: "index_favorites_on_album_id"
   end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -51,5 +44,4 @@ ActiveRecord::Schema.define(version: 2019_12_27_082906) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "favorites", "albums"
 end
